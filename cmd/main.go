2355eb606d31adc7942130e94e86a64bada5f64b
William Koller <williamkoller30@gmail.com>
2025-01-03 01:56:45 -0300

    test: add new tests

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 )
 
-func main() {
+func Run() error {
 	repo := repositories.NewInMemoryOrderRepository()
 	createOrderUseCase := usecases.NewCreateOrderUseCase(repo)
 
@@ -16,12 +16,17 @@ func main() {
 
 	order, err := createOrderUseCase.Execute(orderID, products, totalPrice)
 	if err != nil {
-		fmt.Printf("Error creating order: %v\n", err)
-		return
+		return fmt.Errorf("error creating order: %w", err)
 	}
 
 	fmt.Printf("Order created with ID: %s\n", order.GetID())
 	fmt.Printf("Products: %v\n", order.GetProducts())
-	fmt.Printf("Total price: %v\n", order.GetTotalPrice())
+	fmt.Printf("Total price: %.2f\n", order.GetTotalPrice().GetAmount())
+	return nil
+}
 
+func main() {
+	if err := Run(); err != nil {
+		fmt.Println(err)
+	}
 }
--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,33 +1,10 @@
 package main
 
-import (
-	"ddd-golang/domain/repositories"
-	"ddd-golang/usecases"
-	"testing"
-)
+import "testing"
 
-func TestMainFunction(t *testing.T) {
-	repo := repositories.NewInMemoryOrderRepository()
-	createOrderUseCase := usecases.NewCreateOrderUseCase(repo)
-
-	orderID := "1234"
-	products := []string{"Product A", "Product B"}
-	totalPrice := 100.0
-
-	order, err := createOrderUseCase.Execute(orderID, products, totalPrice)
+func TestRun(t *testing.T) {
+	err := Run()
 	if err != nil {
-		t.Fatalf("Error creating order: %v", err)
-	}
-
-	if order.GetID() != orderID {
-		t.Errorf("Expected order ID %s, got %s", orderID, order.GetID())
-	}
-
-	if len(order.GetProducts()) != len(products) {
-		t.Errorf("Expected products %v, got %v", products, order.GetProducts())
-	}
-
-	if order.GetTotalPrice().GetAmount() != totalPrice {
-		t.Errorf("Expected total price %.2f, got %.2f", totalPrice, order.GetTotalPrice().GetAmount())
+		t.Fatalf("Run() returned an error: %v", err)
 	}
 }
